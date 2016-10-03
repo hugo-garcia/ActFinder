d579b32f9335c40eb1c087410c5a1792c047a60f
Hugo A Garcia <Hugo@hugos-mbp.attlocal.net>
2016-10-03 04:40:29 -0500

    First commit

---
+++ app/src/main/java/com/hugo/actfinder/MainActivity.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright Google Inc. All Rights Reserved.
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hugo.actfinder;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+//            case R.id.search_activities:
+//                Intent dietIntent = new Intent(this, search.class);
+//                startActivity(dietIntent);
+//                break;
+            case R.id.chats:
+                Intent chatIntent = new Intent(this, ChatActivity.class);
+                startActivity(chatIntent);
+                break;
+
+        }
+    }
+}
